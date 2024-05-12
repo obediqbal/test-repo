2854e60cb5feb086cbe7863c2bc92df170c332a5
Lian <121476845+yrubnv@users.noreply.github.com>
2024-05-12 13:57:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,10 @@ def multiply(a, b):
 
 
 # TODO: Implement divide function
+def divide(a, b):
+    return a/b
 
-# orang mati
+# i guess im not good enough for this job
 
 
 x = 10
@@ -21,4 +23,4 @@ y = 5
 print(add(x, y))
 print(subtract(x, y))
 print(multiply(x, y))
-# print(divide(x, y))
+print(divide(x, y))
