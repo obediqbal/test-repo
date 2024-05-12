b1d4f07d101fb353be49ab6db139763f17d5c1df
Rezapu Obed <obed.ubaidillah@gmail.com>
2024-05-12 13:30:57 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    # TODO: Implement this
+    pass
+
+
+# TODO: Implement divide function
+
+
+x = 10
+y = 5
+print(add(x, y))
+print(subtract(x, y))
+print(multiply(x, y))
+# print(divide(x, y))
